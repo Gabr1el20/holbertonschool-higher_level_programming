f67bae6b04e03bd432e476c7df5229537db3ac4c
Gabr1el20 <6327@holbertonstudents.com>
2023-05-30 10:15:06 -0700

    Fixed comparisons

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     "A function that checks for lowercase character"
-    if c >= ord('a') and c <= ord('z'):
+    if c >= chr(97) and c <= chr(122):
         return True
     else:
         return False
