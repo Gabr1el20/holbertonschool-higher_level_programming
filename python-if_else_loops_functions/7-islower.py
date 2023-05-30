8c5310c91432108fd530abd777d1ed2bb3ecf51b
Gabr1el20 <6327@holbertonstudents.com>
2023-05-30 10:04:04 -0700

    Adding task 7 file

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    "A function that checks for lowercase character"
+    if c >= ord('a') and c <= ord('z'):
+        return True
+    else:
+        return False
