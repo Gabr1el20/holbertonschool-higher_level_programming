eed76313e8e82c180ecc6e1912835d538b43d3fe
Gabr1el20 <6327@holbertonstudents.com>
2023-05-30 08:12:53 -0700

    Testing 0 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
+for i in range(0, 99):
     if i < 10:
-        print("0{:d}".format(i), end=', ')
+        print("0{:d}, ".format(i), end='')
     else:
-        print("{:d}".format(i), end=', ')
+        print("{:d}, ".format(i), end='')
+print(99)
