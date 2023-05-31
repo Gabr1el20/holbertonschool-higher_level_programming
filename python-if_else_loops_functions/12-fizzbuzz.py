d50ccfda1a53c3542d4cffd9ce3426a60fe8315f
Gabr1el20 <6327@holbertonstudents.com>
2023-05-31 09:47:06 -0700

    Changing FizzBuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     "A function that prints 1 to 100"
     for i in range(1, 101):
-        if i % 5 == 0 and i % 3 == 0:
+        if i % 15 == 0:
             print("FizzBuzz", end=' ')
         elif i % 3 == 0:
             print("Fizz", end=' ')
