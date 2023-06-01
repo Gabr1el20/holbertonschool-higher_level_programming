e2fa33d767183d69e4df79d8ede05557c18e21ad
Gabr1el20 <6327@holbertonstudents.com>
2023-06-01 06:42:31 -0700

    Handled output format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,11 @@
 from sys import argv
 if __name__ == "__main__":
     i = len(argv) - 1
-    if i != 1:
-        print("{} arguments".format(i))
-    else:
-        print("{} argument".format(i))
+    if i > 1:
+        print("{} arguments:".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    elif i == 0:
+        print("{} arguments.".format(i))
     for i in range(1, len(argv)):
         print("{0}: {1}".format(i, argv[i]))
