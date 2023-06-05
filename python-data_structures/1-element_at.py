57e348881f03aba65b07571a8a822fd51327289e
Gabr1el20 <6327@holbertonstudents.com>
2023-06-05 06:23:27 -0700

    Changing returns

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 def element_at(my_list, idx):
     "A function that retrieves an element from a list"
     if idx < 0:
-        return
+        return None
     elif idx > len(my_list):
-        return
-    x = my_list[idx]
-    return x
+        return None 
+    return my_list[idx]
