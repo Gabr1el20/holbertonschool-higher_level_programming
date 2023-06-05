3f6c092519b8ad8cca9a815ef0d25a66d02551ce
Gabr1el20 <6327@holbertonstudents.com>
2023-06-05 06:58:20 -0700

    Changing format for test

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     "A function that prints all integers of a list, in reverse order"
     for x in reversed(range(len(my_list))):
-        print("{:d}".format(my_list[x]))
+        print("{}".format(my_list[x]))
