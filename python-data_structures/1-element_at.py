6b28270d3ab93291accb9722edb41683045637e4
Gabr1el20 <6327@holbertonstudents.com>
2023-06-05 06:28:29 -0700

    Changin range of elif:

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     "A function that retrieves an element from a list"
     if idx < 0:
         return "None"
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return "None"
     return my_list[idx]
