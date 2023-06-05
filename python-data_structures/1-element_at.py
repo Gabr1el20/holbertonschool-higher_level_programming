8c6a65028c8871bd255243843245b24131821d05
Gabr1el20 <6327@holbertonstudents.com>
2023-06-05 06:29:50 -0700

    Testing return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     "A function that retrieves an element from a list"
     if idx < 0:
-        return "None"
+        return
     elif idx > len(my_list) - 1:
-        return "None"
+        return
     return my_list[idx]
