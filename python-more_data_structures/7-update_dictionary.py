c869ab127d54d659daca4c1114adcede30dc132e
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 12:13:24 -0700

    Adding 'update_dictionary'

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     "A function that replaces or adds key/value in a dictionary"
-    for k, v in a_dictionary.items():
-        if k == key:
-            a_dictionary[k] = value
-        else:
-            a_dictionary[key] = value
+    if a_dictionary is None:
+        return
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
