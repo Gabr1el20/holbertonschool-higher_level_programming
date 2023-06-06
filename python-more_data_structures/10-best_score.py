9ba7a97aa047302507b4ddb7c6fdc8afdbcb7460
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 12:51:38 -0700

    Didn´t work, reboot

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     "A function that returns a key with the biggest int value"
-    if a_dictionary is None:
-        return
+    if a_dictionary:
+        return max(a_dictionary, a_dictionary.get)
     else:
-        maxValue = max(a_dictionary)
-        maxKey = max(a_dictionary, key=a_dictionary.get)
-        return maxKey
+        return
