0f0bd580a3c59cdeaec55b6f5a64d7c48301f15e
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 08:02:44 -0700

    fsbhf

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None or len(matrix) == 0:
         return
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][column]), end="")
-            if column < len(matrix[row]) - 1:
+    for column in range(len(matrix)):
+        for row in range(len(matrix[column])):
+            print("{:d}".format(matrix[column][row]), end="")
+            if row < len(matrix[column]) - 1:
                 print(end=" ")
         print()
