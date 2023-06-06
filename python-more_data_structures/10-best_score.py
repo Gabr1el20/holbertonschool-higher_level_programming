cd456db09f40320403c66247cf26862981972d3d
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 12:44:39 -0700

    Added 'biggest integer value'

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    "A function that returns a key with the biggest int value"
+    if a_dictionary is None:
+        return
+    maxKey = max(a_dictionary, key=a_dictionary.get)
+    return maxKey
