d52eb6316cd240d2e5743ade6b32e45c32ec88f3
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 11:59:51 -0700

    Pure testing

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     "A function that replaces or adds key/value in a dictionary"
     for k, v in a_dictionary.items():
         if k == key:
-            v = value
+            a_dictionary[k] = value
         else:
             a_dictionary[key] = value
