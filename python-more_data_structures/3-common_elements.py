8d3bfa40d78e456cb745d68fdbab081100e1fb61
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 11:19:15 -0700

    Adding common elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    "A function that returns a set of common elements in two sets"
+    on3 = set(set_1)
+    tw0 = set(set_2)
+    return (on3 & tw0)
