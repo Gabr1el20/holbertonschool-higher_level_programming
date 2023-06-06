9d304bd605e0b8882cb5508fca6502b68fdca5a5
Gabr1el20 <6327@holbertonstudents.com>
2023-06-06 06:04:10 -0700

    Adding 'find the max'

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    "A function that finds the biggest integer of a list"
+    if len(my_list) == 0:
+        return
+    biggest = None
+    for comparator in my_list:
+        if biggest == None or biggest < comparator:
+            biggest = comparator
+    return biggest
