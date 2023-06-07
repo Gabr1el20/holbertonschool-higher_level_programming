64fe3f544ea677059ea556ed3964bf74e8e3016c
Gabr1el20 <6327@holbertonstudents.com>
2023-06-07 08:23:56 -0700

    Testing

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     "A function that converts a roman Numeral to integer"
-    dicto = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     romanInt = 0
-    for letter in roman_string:
-        for k, v in dicto.items():
-            if k == letter:
-                romanInt += v
+    rs = roman_string
+    if type(rs) is not str or rs is None:
+        return 0
+    for x in range(len(rs)):
+        for k, v in d.items():
+            if k == rs[x]:
+                if x < len(rs) - 1 and v < d.get(rs[x + 1]):
+                    romanInt -= v
+                else:
+                    romanInt += v
     return romanInt
