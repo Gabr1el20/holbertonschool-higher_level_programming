4e9551847ea50329736d0627c497d06dd00c4bd0
Gabr1el20 <6327@holbertonstudents.com>
2023-06-07 09:41:06 -0700

    Cleanse of more-data-structures

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     "A function that prints a dictionary by ordered keys"
+    if a_dictionary is None:
+        return
     for k, v in sorted(a_dictionary.items()):
         print("{}: {}".format(k, v))
