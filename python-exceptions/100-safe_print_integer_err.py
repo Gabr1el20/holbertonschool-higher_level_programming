68fa40e69e297a7707a9d0ee274511e05e6bd949
Gabr1el20 <6327@holbertonstudents.com>
2023-06-08 12:44:30 -0700

    Added TypeError as exception

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,7 @@ def safe_print_integer_err(value):
             print("{:d}".format(value))
         else:
             print("{:d}".format(value))
-    except ValueError as e:
+    except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:
