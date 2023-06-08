5dbf54098ae52f98472fc929805dc26779431422
Gabr1el20 <6327@holbertonstudents.com>
2023-06-08 11:12:58 -0700

    Added list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    'Divides element by element 2 lists'
+    newList = []
+    errorList = []
+    for i in range(list_length):
+        try:
+            division = my_list_1[i] / my_list_2[i]
+            newList.append(division)
+        except ZeroDivisionError:
+            errorList.append("division by 0")
+            newList.append(0)
+        except TypeError:
+            errorList.append("wrong type")
+            newList.append(0)
+        except IndexError:
+            errorList.append("out of range")
+            newList.append(0)
+        finally:
+            pass
+    for e in errorList:
+        print(e)
+    return newList
