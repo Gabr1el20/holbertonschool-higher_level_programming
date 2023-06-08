4c179567fb22a79cc68b44687d9712f4d2496b51
Gabr1el20 <6327@holbertonstudents.com>
2023-06-08 05:25:16 -0700

    Adding safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     "Prints x elements of a list"
-    #First, try to print the members of my_list in the range of x
+    count = 0
     try:
         for num in range(x):
             print("{}".format(my_list[num]), end="")
-    #If an execption occurs, update the value of x to be equal to the lenght of my_list
+            count += 1
     except Exception:
         pass
-    #Finally, print a None (to handle the \n) and return x
     finally:
         print()
-        return x
+        return count
