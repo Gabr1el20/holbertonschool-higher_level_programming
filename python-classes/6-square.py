bfb654c605b70c44fe88a2b56d6650171d5d40cd
Gabr1el20 <6327@holbertonstudents.com>
2023-06-12 12:13:30 -0700

    MORE tests

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,6 +37,10 @@ class Square:
     @size.setter
     def size(self, value):
         "Setter of the size"
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
         self.__size = value
 
     def area(self):
