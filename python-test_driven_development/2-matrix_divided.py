377fd2f5997e52b5287a23f7a81568a1a8dbde48
Gabr1el20 <6327@holbertonstudents.com>
2023-06-13 11:00:23 -0700

    Testing task 1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    "Divided a matrix"
+    errore = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    lengi = len(matrix[0])
+    dividedMat = []
+    for i in matrix:
+        for j in i:
+            if not isinstance(j, (int, float)):
+                raise TypeError(errore)
+    for row in matrix:
+        dividedrow = []
+        for each in row:
+            dividedElem = round(each / div, 2)
+            dividedrow.append(dividedElem)
+        dividedMat.append(dividedrow)
+    if len(row) != lengi:
+        raise TypeError("Each row of the matrix must have the same size")
+    return dividedMat
