6aa8026aa7a979f7c8383a2cca8dd9678d8498b9
Gabr1el20 <6327@holbertonstudents.com>
2023-06-14 09:39:33 -0700

    Testing text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+def text_indentation(text):
+    '''Prints a text with 2 new lines after each of
+    these character: ".", "?" and ":"'''
+    SpaceCheck = False
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i - 1] in {".", "?", ":"}:
+            print()
+            print()
+            SpaceCheck = True
+        if SpaceCheck:
+            if i < len(text) - 1 and text[i + 1] == " ":
+                continue
+            SpaceCheck = False
+        else:
+            print(text[i], end="")
