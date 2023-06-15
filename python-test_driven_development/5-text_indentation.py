70cdfedeb296df49f6380eccb9169844ce33d352
Gabr1el20 <6327@holbertonstudents.com>
2023-06-15 08:57:33 -0700

    Let me see

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"Task 4"
+'''Task 4'''
 
 
 def text_indentation(text):
     '''Prints a text with 2 new lines after each of
     these character: ".", "?" and ":"'''
     SpaceCheck = False
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in range(len(text)):
         if text[i - 1] in {".", "?", ":"}:
-            print(text[i] + "\n")
+            print()
+            print()
             SpaceCheck = True
         if SpaceCheck:
             if i < len(text) - 1 and text[i + 1] == " ":
