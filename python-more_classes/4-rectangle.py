9a10625a51e85d83df5025b229e1603d7fc08956
Gabr1el20 <6327@holbertonstudents.com>
2023-06-15 11:51:36 -0700

    Added a space in __repr__

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -69,4 +69,4 @@ class Rectangle():
 
     def __repr__(self):
         "Representation of the rectangle"
-        return f'Rectangle({self.__width},{self.__height})'
+        return f'Rectangle({self.__width}, {self.__height})'
