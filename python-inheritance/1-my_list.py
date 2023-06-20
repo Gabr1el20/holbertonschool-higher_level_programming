9f24c58b7f75337e614be189aade8c49ad5d2e6b
Gabr1el20 <6327@holbertonstudents.com>
2023-06-20 08:47:38 -0700

    Testing

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,4 +6,4 @@ class MyList(list):
     "A class MyList that inherits from list"
     def print_sorted(self):
         "Prints the list, but sorted"
-        print("{}".format(sorted(self)))
+        print(sorted(self))
