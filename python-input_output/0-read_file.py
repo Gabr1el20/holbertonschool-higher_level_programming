481b86982657e3961887d181c9c1c9cc3439f167
Gabr1el20 <6327@holbertonstudents.com>
2023-06-20 13:27:18 -0700

    idk

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     "Reads a text file and prints it to stdout"
     with open(filename, encoding='utf-8') as fileR:
         content = fileR.read()
-        print(content)
+        print(content, end="")
