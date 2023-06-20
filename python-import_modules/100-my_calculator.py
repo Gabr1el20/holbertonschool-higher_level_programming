b38cb5a79aa314f6f0cc2d98642c947f4c29ffed
Gabr1el20 <6327@holbertonstudents.com>
2023-06-20 05:27:36 -0700

    Changing task 27 answer

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
         if op == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
         if op == "*":
-            print("{} * {:d} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         if op == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
