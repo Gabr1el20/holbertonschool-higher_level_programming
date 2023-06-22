35c420e0d9b5e8c196fe9987c89c14226981e08d
Gabr1el20 <6327@holbertonstudents.com>
2023-06-22 05:47:20 -0700

    dump()

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     "placeholder"
-    jsoned = json.dumps(my_obj)
     with open(filename, mode='w') as j_son:
-        j_son.write(jsoned)
+        json.dump(my_obj, j_son)
