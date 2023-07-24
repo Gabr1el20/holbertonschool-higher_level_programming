7b5092a64bfa645660b8e1960098c95969f56d20
Gabr1el20 <6327@holbertonstudents.com>
2023-07-24 08:31:10 -0700

    Task 16

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -16,3 +16,9 @@ VALUES (3, 'Bob', 14);
 
 INSERT INTO second_table (id, name, score)
 VALUES (4, 'George', 8);
+
+INSERT INTO second_table (id, name, score)
+VALUES (5, 'Aria', 18);
+
+INSERT INTO second_table (id, name, score)
+VALUES (6, 'Aria', 12);
