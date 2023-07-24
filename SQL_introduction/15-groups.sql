e834a00c1085b14fc2cc13ff7c7e5c8e6fc86dde
Gabr1el20 <6327@holbertonstudents.com>
2023-07-24 08:18:34 -0700

    Task 15

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Lists the number of records with the same score
+-- In the table second_table of the databse hbtn_0c_0
+-- In your MySQL server.
+SELECT score, COUNT(*) as number FROM second_table
+GROUP BY score HAVING COUNT(*) >= 1;
