98ad2ae41c079114262c25dbe600541f5de92fc3
Gabr1el20 <6327@holbertonstudents.com>
2023-07-25 08:50:25 -0700

    Test

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- Lists all the cities of California
 -- That can be found in the db hbtn_0d_usa.
 SELECT id, name FROM cities
+-- Subquery for get the id of the state.
 WHERE state_id = (SELECT id from states WHERE name = "California")
 ORDER BY id;
