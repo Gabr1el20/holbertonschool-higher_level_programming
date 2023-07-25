cbc98a6b59eb0c3dee4edb5f85bca9849d9b122e
Gabr1el20 <6327@holbertonstudents.com>
2023-07-25 06:07:46 -0700

    NADJ

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California
 -- That can be found in the db hbtn_0d_usa.
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id from states WHERE name = 'California')
+WHERE state_id = (SELECT id from states WHERE name = "San FRANCISCO")
 ORDER BY cities.id ASC;
