48549feaf208ef8cf81fa5d9304d38bf636b3f88
Gabr1el20 <6327@holbertonstudents.com>
2023-07-25 10:42:00 -0700

    Task 16

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@ DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 ALTER USER IF EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE, SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT USAGE, SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
