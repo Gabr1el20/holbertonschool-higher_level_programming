93f3ae9be6a225d67de70827996011806c688d28
Gabr1el20 <6327@holbertonstudents.com>
2023-07-26 12:41:43 -0700

    Test of task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"This script lists all states starting with an 'N' from the db 'hbtn_0e_usa'"
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+
+    cur.execute('''SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON state_id = states.id
+                ORDER BY cities.id''')
+    fetchs = cur.fetchall()
+    for x in fetchs:
+        print(x)
+
+    db.close()
