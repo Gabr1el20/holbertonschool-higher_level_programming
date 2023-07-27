b1a23aa58d9998dd2b403511a460e26cd8f96e71
Gabr1el20 <6327@holbertonstudents.com>
2023-07-27 05:52:17 -0700

    Task 9 testing

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"Lists all State objects from the database hbtn_0e_6_usa"
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                            .format(argv[1], argv[2], argv[3])))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = (
+        session.query(State).filter(State.name.ilike("%a")).order_by(State.id)
+    )
+    for x in states:
+        print(x.id, end='')
+        print(": ", end='')
+        print(x.name)
