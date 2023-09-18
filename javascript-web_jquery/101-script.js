c0d8a49ede254792b11f9d492a4936f39c7c2c24
Gabr1el20 <6327@holbertonstudents.com>
2023-09-18 12:03:47 -0700

    Task 10 & 11

---
+++ javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#add_item').click(() => {
+        $('.my_list').append('<li>Item</li>');
+    })
+    $('#remove_item').click(() => {
+        $('.my_list li:last-child').remove();
+    })
+    $('#clear_list').click(() => {
+        $('.my_list li').remove();
+    })
+})
